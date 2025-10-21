f4c123254b4e486470e8832a6cb452f343d9fb79
nguyen ha trung <hatrungngn@gmail.com>
2025-10-21 21:07:14 +0700

    feature/change-password

---
+++ socket/socketHandler.js
@@ -0,0 +1,26 @@
+import { Server } from "socket.io";
+
+let io;
+
+function initSocket(server) {
+  io = new Server(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("Client connected:", socket.id);
+
+    socket.on("disconnect", () => {
+      console.log("Client disconnected:", socket.id);
+    });
+  });
+}
+
+function getIO() {
+  if (!io) throw new Error("Socket.io chưa được khởi tạo");
+  return io;
+}
+
+export { initSocket, getIO };
