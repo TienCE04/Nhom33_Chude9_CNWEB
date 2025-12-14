8980ed655ddf57ced457156c626ab63d01356fb6
nguyen ha trung <hatrungngn@gmail.com>
2025-12-14 23:38:27 +0700

    update logic roomController

--- Backend/routes/roomRouter.js
+++ Backend/routes/roomRouter.js
@@ -5,10 +5,11 @@ const { authorize } = require("../middleware");
 const router = Router();
 
 router.post("/rooms", authorize, roomController.createRoom);
-router.get("/rooms", authorize, roomController.listRooms);
-router.get("/rooms/:roomId", authorize, roomController.getRoomById);
+router.get("/rooms",  roomController.listRooms);
+router.get("/rooms/:roomId",  roomController.getRoomById);
 router.delete("/rooms/:roomId", authorize, roomController.deleteRoom);
 router.get('/rooms/:username', authorize, roomController.getRoomForUser);
+
 module.exports = router;
 
 
