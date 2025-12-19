980762bc7d7b29b0c6061d4e93c264c5534b0a93
LUONGTHUTRANG <trang.lt225102@sis.hust.edu.vn>
2025-12-19 16:13:55 +0700

    add navigate for route not exist

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Notification } from "@/components/Notification";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "@/components/Layout";
 import ProtectedRoute from "@/components/ProtectedRoute";
 
@@ -57,7 +57,7 @@ const App = () => {
             </Route>
 
             <Route path="/game" element={<ProtectedRoute element={<Game />} />} />
-            <Route path="*" element={<NotFound />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
